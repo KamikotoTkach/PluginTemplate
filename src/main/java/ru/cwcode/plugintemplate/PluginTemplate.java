fcb62e9a59d2a300f10e8cd57d4aaded29955ba7
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-08-17 16:24:18 +0300

    I think done

--- src/main/java/ru/cwcode/plugintemplate/PluginTemplate.java
+++ src/main/java/ru/cwcode/plugintemplate/PluginTemplate.java
@@ -1,17 +1,101 @@
 package ru.cwcode.plugintemplate;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import tkachgeek.config.yaml.YmlConfig;
+import tkachgeek.config.yaml.YmlConfigManager;
+import tkachgeek.tkachutils.bootstrap.Bootstrap;
+import tkachgeek.tkachutils.reflection.ClassScanner;
+import tkachgeek.tkachutils.reflection.ReflectionUtils;
+import tkachgeek.tkachutils.reflection.injector.InjectFields;
+import tkachgeek.tkachutils.reflection.injector.Injector;
+import tkachgeek.tkachutils.scheduler.Tasks;
+import tkachgeek.tkachutils.scheduler.annotationRepeatable.Repeat;
+import tkachgeek.tkachutils.scheduler.annotationRepeatable.RepeatAPI;
 
-public final class PluginTemplate extends JavaPlugin {
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+public abstract class PluginTemplate extends Bootstrap {
+  public static YmlConfigManager yml;
+  public static JavaPlugin plugin;
+  public static Logger logger;
+  List<Runnable> runSync = new ArrayList<>();
+  
+  @Override
+  public void onDisable() {
+    yml.storeAll();
+    Tasks.cancelTasks(this);
+  }
+  
+  @Override
+  public void onLoad() {
+    plugin = this;
+    logger = getLogger();
+    yml = new YmlConfigManager(this);
+    
+    super.onLoad();
+  }
+  
+  @Override
+  protected CompletableFuture<Void> asyncTask() {
+    InjectFields injectFields = getInjectFields();
+    
+    injectFields.bind(logger, Logger.class);
+    injectFields.bind(plugin, JavaPlugin.class);
+    
+    return CompletableFuture.runAsync(() -> {
+      
+      new ClassScanner(this.getFile())
+         .apply(new ClassScanner.ClassApplier(YmlConfig.class::isAssignableFrom,
+                                              classInfo -> ReflectionUtils.tryToInvokeStaticMethod(classInfo, "getInstance")))
+         
+         .apply(new ClassScanner.ClassApplier(Listener.class::isAssignableFrom,
+                                              this::registerListener))
+         
+         .apply(new ClassScanner.ClassApplier(classInfo -> true,
+                                              classInfo -> Injector.inject(classInfo, injectFields)))
+         
+         .apply(new ClassScanner.MethodApplier(method -> Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0,
+                                               this::handleRepeatMethod))
+         
+         .scan(this);
+    });
+  }
+  
+  @Override
+  public void onEnable() {
+    super.onEnable();
+    runSync.forEach(Runnable::run);
+    runSync = null;
+  }
+  
+  @NotNull
+  protected InjectFields getInjectFields() {
+    return new InjectFields();
+  }
+  
+  protected void registerListener(Class<?> classInfo) {
+    Object listener = ReflectionUtils.getNewInstance(classInfo);
+    if(listener == null) return;
+    
+    Bukkit.getPluginManager().registerEvents((Listener) listener, this);
+  }
+  
+  protected void handleRepeatMethod(Method method) {
+    Repeat annotation = method.getAnnotation(Repeat.class);
+    
+    if (annotation != null) {
+      runSync.add(() -> {
+        this.getLogger().info("Registered task " + method.getDeclaringClass().getSimpleName() + "/" + method.getName());
+        RepeatAPI.registerRepeatable(this, method, annotation);
+      });
     }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+  }
 }
