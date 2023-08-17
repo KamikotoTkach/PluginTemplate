4ab5592f8ea3ecaa88e1d70c4b87ee95dd934056
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-08-17 15:30:24 +0300

    Initial commit

---
+++ src/main/java/ru/cwcode/plugintemplate/PluginTemplate.java
@@ -0,0 +1,17 @@
+package ru.cwcode.plugintemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTemplate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
